b572e65eb3184a628a6e331b852ab88e90e06cca
nyoshioka <nyoshioka.1120@gmail.com>
2024-05-10 12:52:32 +0900

    マップデータ読み込み追加

--- Assets/Script/MapDataController.cs
+++ Assets/Script/MapDataController.cs
@@ -13,37 +13,38 @@ public class MapDataController : MonoBehaviour
     [SerializeField] Tilemap tilemap;
     [SerializeField] TileDataList tile_data_list;
 
-    [SerializeField] string FILE_NAME = "map_data.json";
-    [SerializeField] string FILE_DIR = "Assets/MapData/";
+    [SerializeField] GameObject empty_grid_prefab;
+
+    [SerializeField] string FILE_NAME = "map_data";
+    [SerializeField] string FILE_DIR = "MapData/";
 
     // Start is called before the first frame update
     void Start()
     {
         Save();
+        Load();
     }
 
     public void Save()
     {
-        // 反転回転の取得
-        // TilemapのGetTransformMatrix(Vector3Int).rotation
-
-        // 通常         Quaternion(0f,0f,0f,1f)
-        // X軸反転      Quaternion(0.0, 1.0, 0.0, 0.0)
-        // Y軸反転	    Quaternion(1.0, 0.0, 0.0, 0.0)
-        // 90度回転     Quaternion(0.0, 0.0, -0.7, 0.7)
-        // 180度回転	Quaternion(0.0, 0.0, 1.0, 0.0)
-        // 270度回転	Quaternion(0.0, 0.0, 0.7, 0.7)
+        if(tilemap == null)
+        {
+            Debug.Log("[warning]tilemap not exist");
+            return;
+        }
 
         tilemap.CompressBounds();
-        var b = tilemap.cellBounds;
+        var cell_bounds = tilemap.cellBounds;
 
-        List<MapDatas> list = new List<MapDatas>();
+        MapDatas list = new MapDatas();
+        list.setting.origin = tilemap.origin;
+        list.setting.size = tilemap.size;
 
-        for(int y = b.min.y; y < b.max.y; y++)
+        for(int y = cell_bounds.min.y; y < cell_bounds.max.y; y++)
         {
-            MapDatas tile_datas = new MapDatas();
+            List<MapData> tile_datas = new List<MapData>();
 
-            for(int x = b.min.x; x < b.max.x; x++)
+            for(int x = cell_bounds.min.x; x < cell_bounds.max.x; x++)
             {
                 MapData tile_data = new MapData();
 
@@ -56,10 +57,10 @@ public class MapDataController : MonoBehaviour
                     tile_data.rot = rot;                  
                 }
 
-                tile_datas.data_list.Add(tile_data);
+                tile_datas.Add(tile_data);
             }
 
-            list.Add(tile_datas);
+            list.data_list.Add(tile_datas);
         }
 
         string json = JsonMapper.ToJson(list);
@@ -70,22 +71,68 @@ public class MapDataController : MonoBehaviour
             Directory.CreateDirectory(FILE_DIR);
         }
 
-        string path = Path.Combine(FILE_DIR + FILE_NAME);
+        string path = Path.Combine("Assets/Resources/" + FILE_DIR + FILE_NAME + ".json");
 
         StreamWriter writer = new StreamWriter(path, false);
         writer.WriteLine(json);
         writer.Flush();
         writer.Close();
     }
+
+    public void Load()
+    {
+        if(tilemap)
+        {
+            tilemap.ClearAllTiles();
+        }
+        else
+        {
+            var grid = Instantiate(empty_grid_prefab);
+            tilemap = grid.transform.GetChild(0).GetComponent<Tilemap>();
+        }
+
+        string json = Resources.Load(FILE_DIR + FILE_NAME).ToString();
+
+        var json_data = JsonMapper.ToObject<MapDatas>(json);
+
+        int y = (int)json_data.setting.origin.y;
+        foreach(var data_list in json_data.data_list)
+        {
+            int x = (int)json_data.setting.origin.x;
+            foreach(var map_data in data_list)
+            {
+                if(map_data.name == "")
+                {
+                    x++;
+                    continue;
+                }
+                else
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tile_data_list.list.Single(t => t.name == map_data.name).tile);
+                    Matrix4x4 mat = Matrix4x4.TRS(Vector3.zero, map_data.rot, Vector3.one);
+                    tilemap.SetTransformMatrix(new Vector3Int(x, y, 0), mat);
+                    x++;
+                }
+            }
+            y++;
+        }
+    }
 }
 
 public class MapDatas
 {
-    public List<MapData> data_list = new List<MapData>();
+    public MapSetting setting = new MapSetting();
+    public List<List<MapData>> data_list = new List<List<MapData>>();
+}
+
+public class MapSetting
+{
+    public Vector3 origin = new Vector3(0, 0, 0);
+    public Vector3 size = new Vector3(0, 0, 0);
 }
 
 public class MapData
 {
     public string name = "";
-    public Quaternion rot = new Quaternion(0, 0, 0, 0);
+    public Quaternion rot = new Quaternion(0, 0, 0, 1.0f);
 }
