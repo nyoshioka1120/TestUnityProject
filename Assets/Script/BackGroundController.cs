b0789e651455f34d5836d9733e5e853462d8fdf9
nyoshioka <nyoshioka.1120@gmail.com>
2024-05-16 19:07:57 +0900

    遠近で背景を分離

--- Assets/Script/BackGroundController.cs
+++ Assets/Script/BackGroundController.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class BackGroundController : MonoBehaviour
 {
-    [SerializeField] GameObject m_bg_prefab;
+    [SerializeField] GameObject m_bg_far_prefab;
+    [SerializeField] GameObject m_bg_near_prefab;
 
     GameObject m_camera;
     Vector3 m_camera_pos = new Vector3(0, 0, 0);
+    Vector3 m_camera_pos_pre = new Vector3(0, 0, 0);
 
-    GameObject m_bg_filter_1;
+    GameObject m_bg_filter_far;
+    GameObject m_bg_filter_near;
 
     GameObject m_bg_1;
     GameObject m_bg_2;
     GameObject m_bg_3;
+
+    List<GameObject> m_bg_far;
+    List<GameObject> m_bg_near;
 
     const float BG_W = 18.0f;
 
@@ -22,46 +28,81 @@ public class BackGroundController : MonoBehaviour
     {
         m_camera = GameObject.Find("Main Camera");
         m_camera_pos = m_camera.transform.position;
+        m_camera_pos_pre = m_camera_pos;
 
-        m_bg_filter_1 = GameObject.Find("BackGroundFilter1");
-        m_bg_filter_1.transform.position = new Vector3(m_camera_pos.x, m_camera_pos.y, 1.0f);
+        m_bg_filter_far = GameObject.Find("BackGroundFilterFar");
+        m_bg_filter_far.transform.position = new Vector3(m_camera_pos.x, m_camera_pos.y, 3.0f);
 
-        m_bg_1 = Instantiate(m_bg_prefab);
-        m_bg_1.transform.position = new Vector3(m_camera_pos.x, m_camera_pos.y, 2.0f);
+        m_bg_far = new List<GameObject>();
 
-        m_bg_2 = Instantiate(m_bg_prefab);
-        m_bg_2.transform.position = new Vector3(m_camera_pos.x + BG_W, m_camera_pos.y, 2.0f);
+        var bg_far_1 = Instantiate(m_bg_far_prefab);
+        bg_far_1.transform.position = new Vector3(m_camera_pos.x, m_camera_pos.y, 4.0f);
+        m_bg_far.Add(bg_far_1);
 
-        m_bg_3 = Instantiate(m_bg_prefab);
-        m_bg_3.transform.position = new Vector3(m_camera_pos.x - BG_W, m_camera_pos.y, 2.0f);
+        var bg_far_2 = Instantiate(m_bg_far_prefab);
+        bg_far_2.transform.position = new Vector3(m_camera_pos.x + BG_W, m_camera_pos.y, 4.0f);
+        m_bg_far.Add(bg_far_2);
+
+        var bg_far_3 = Instantiate(m_bg_far_prefab);
+        bg_far_3.transform.position = new Vector3(m_camera_pos.x - BG_W, m_camera_pos.y, 4.0f);
+        m_bg_far.Add(bg_far_3);
+
+
+        m_bg_filter_near = GameObject.Find("BackGroundFilterNear");
+        m_bg_filter_near.transform.position = new Vector3(m_camera_pos.x, m_camera_pos.y, 1.0f);
+
+        m_bg_near = new List<GameObject>();
+
+        var bg_near_1 = Instantiate(m_bg_near_prefab);
+        bg_near_1.transform.position = new Vector3(m_camera_pos.x, m_camera_pos.y, 2.0f);
+        m_bg_near.Add(bg_near_1);
+
+        var bg_near_2 = Instantiate(m_bg_near_prefab);
+        bg_near_2.transform.position = new Vector3(m_camera_pos.x + BG_W, m_camera_pos.y, 2.0f);
+        m_bg_near.Add(bg_near_2);
+
+        var bg_near_3 = Instantiate(m_bg_near_prefab);
+        bg_near_3.transform.position = new Vector3(m_camera_pos.x - BG_W, m_camera_pos.y, 2.0f);
+        m_bg_near.Add(bg_near_3);
     }
 
     // Update is called once per frame
     void Update()
     {
         m_camera_pos = m_camera.transform.position;
-        m_bg_filter_1.transform.position = new Vector3(m_camera_pos.x, m_camera_pos.y, 1.0f);
 
-        Vector3 bg_pos_1 = new Vector3(m_camera_pos.x / BG_W * BG_W + BG_W, m_camera_pos.y, 2.0f);
-        Vector3 bg_pos_2 = new Vector3(m_camera_pos.x / BG_W * BG_W - BG_W, m_camera_pos.y, 2.0f);
+        m_bg_filter_far.transform.position = new Vector3(m_camera_pos.x, m_camera_pos.y, 3.0f);
+        m_bg_filter_near.transform.position = new Vector3(m_camera_pos.x, m_camera_pos.y, 1.0f);
+
+        Vector3 bg_pos_far_1 = new Vector3(m_camera_pos.x / BG_W * BG_W + BG_W, m_camera_pos.y, 4.0f);
+        Vector3 bg_pos_far_2 = new Vector3(m_camera_pos.x / BG_W * BG_W - BG_W, m_camera_pos.y, 4.0f);
 
         float length = 0;
-        length = Mathf.Abs(m_camera_pos.x - m_bg_1.transform.position.x);
-        if(length >= BG_W * 2)
+
+        foreach(var bg in m_bg_far)
         {
-            m_bg_1.transform.position = m_camera_pos.x > m_bg_1.transform.position.x ? bg_pos_1 : bg_pos_2;
+            Vector3 add = (m_camera_pos - m_camera_pos_pre) * 0.5f;
+            bg.transform.position += add;
+
+            length = Mathf.Abs(m_camera_pos.x - bg.transform.position.x);
+            if(length >= BG_W * 2)
+            {
+                bg.transform.position = m_camera_pos.x > bg.transform.position.x ? bg_pos_far_1 : bg_pos_far_2;
+            }
         }
 
-        length = Mathf.Abs(m_camera_pos.x - m_bg_2.transform.position.x);
-        if(length >= BG_W * 2)
+        Vector3 bg_pos_near_1 = new Vector3(m_camera_pos.x / BG_W * BG_W + BG_W, m_camera_pos.y, 2.0f);
+        Vector3 bg_pos_near_2 = new Vector3(m_camera_pos.x / BG_W * BG_W - BG_W, m_camera_pos.y, 2.0f);
+
+        foreach(var bg in m_bg_near)
         {
-            m_bg_2.transform.position = m_camera_pos.x > m_bg_2.transform.position.x ? bg_pos_1 : bg_pos_2;
+            length = Mathf.Abs(m_camera_pos.x - bg.transform.position.x);
+            if(length >= BG_W * 2)
+            {
+                bg.transform.position = m_camera_pos.x > bg.transform.position.x ? bg_pos_near_1 : bg_pos_near_2;
+            }
         }
 
-        length = Mathf.Abs(m_camera_pos.x - m_bg_3.transform.position.x);
-        if(length >= BG_W * 2)
-        {
-            m_bg_3.transform.position = m_camera_pos.x > m_bg_3.transform.position.x ? bg_pos_1 : bg_pos_2;
-        }
+        m_camera_pos_pre = m_camera_pos;
     }
 }
